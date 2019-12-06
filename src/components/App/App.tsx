bd59e514c2258bbacacbf2dc19d42f5168586d3e
BakayAlexey <bakaylexa@gmail.com>
2019-12-06 18:00:01 +0200

    add App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <header>
+          <a href="">Login with trello account</a>
+        </header>
+        <h2>Please login</h2>
+      </div>
+    );
+  }
+}
